9b49a24ab76d31579e756c39014cc5a1385749a2
Bastyl <bastian.lopez@mail.udp.cl>
2020-12-09 23:15:21 -0300

    Add files via upload

---
+++ bus/c.py
@@ -0,0 +1,45 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+
+server_address = ('200.14.84.235', 5000)
+print('starting up on {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+
+def generate_tx_length(tx_length):
+    char_ammount = 5 # Cantidad de caracteres máximos para definir el largo de la transacción (5 según formato solicitado por el bus)
+    char_left = char_ammount - len(str(tx_length)) # Espacios sobrantes para rellenar con 0 a la izquierda
+
+    str_tx_length = ''
+    for i in range(char_left):
+      str_tx_length += '0'
+    
+    str_tx_length += str(tx_length) # String con el largo de la transacción
+    return str_tx_length
+
+service_name = 'abcde'
+
+tx_cmd = 'getsv'+ service_name  # Comando de registro de servicio ante el bus
+tx = generate_tx_length(len(tx_cmd)) + tx_cmd
+
+print(tx)
+
+sock.send(tx.encode(encoding='UTF-8'))
+
+status = sock.recv(4096).decode('UTF-8')[10:12] # 'OK' (exitoso) o 'NK' (fallido)
+
+print(status)
+print("_______")
+tx_cmd = service_name + 'hola' # Comando de registro de servicio ante el bus
+tx = generate_tx_length(len(tx_cmd)) + tx_cmd
+
+sock.send(tx.encode(encoding='UTF-8'))
+
+status = sock.recv(4096).decode('UTF-8')[10:12] 
+
+print(status)
